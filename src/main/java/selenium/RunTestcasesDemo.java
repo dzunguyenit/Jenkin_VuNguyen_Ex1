9d462cf1879a6d3917756da1657d29ce33672b4f
nguye <nguye@192.168.133.2>
2018-04-02 21:45:14 +0700

    sdf

---
+++ src/main/java/selenium/RunTestcasesDemo.java
@@ -0,0 +1,41 @@
+package selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class RunTestcasesDemo {
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.get("http://demo.guru99.com/v4/");
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void TC_01_DemoRunOnJenkins() {
+		String loginTitle = driver.getTitle();
+		Assert.assertEquals("Guru99 Bank Home Page", loginTitle);
+		
+		WebElement userTxt = driver.findElement(By.xpath("//input[@name='uid']"));
+		Assert.assertTrue(userTxt.isDisplayed());
+	}
+
+	@AfterClass
+	public void afterClass() {
+		// Update code for run CI Jenkin
+		driver.quit();
+	}
+
+}
